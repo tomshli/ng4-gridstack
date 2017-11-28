3918b17a7129b5c9d596c10ccfa5214c55d9c5c6
Thomas Li <tli@whitecapcanada.com>
2017-11-27 19:44:00 -0500

    Remove SampleComponent

--- src/grid-stack.component.spec.ts
+++ src/grid-stack.component.spec.ts
@@ -1,27 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By }              from '@angular/platform-browser';
 import { DebugElement }    from '@angular/core';
-
-import { SampleComponent } from './sample.component';
+import {} from 'jasmine';
 
 describe('SampleComponent', () => {
 
-  let comp:    SampleComponent;
-  let fixture: ComponentFixture<SampleComponent>;
   let de:      DebugElement;
   let el:      HTMLElement;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ SampleComponent ], // declare the test component
+      declarations: [ ], // declare the test component
     });
 
-    fixture = TestBed.createComponent(SampleComponent);
-
-    comp = fixture.componentInstance; // BannerComponent test instance
-
-    // query for the title <h1> by CSS element selector
-    de = fixture.debugElement.query(By.css('h1'));
     el = de.nativeElement;
   });
 
