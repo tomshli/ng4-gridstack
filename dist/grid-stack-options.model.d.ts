76782976c1d45ed5384ee32f00d23da7050b5d2c
Thomas Li <TLi@whitecapcanada.com>
2021-01-07 12:48:20 -0500

    Upgrade to Angular 9

---
+++ dist/grid-stack-options.model.d.ts
@@ -0,0 +1,29 @@
+import 'gridstack/dist/gridstack';
+export declare class GridStackOptions implements GridStackOptions {
+    acceptWidgets: boolean;
+    alwaysShowResizeHandle: boolean;
+    animate: boolean;
+    auto: boolean;
+    cellHeight: number;
+    ddPlugin: boolean;
+    disableDrag: boolean;
+    disableResize: boolean;
+    draggable: boolean;
+    handle: string;
+    handleClass: string;
+    height: number;
+    float: boolean;
+    itemClass: string;
+    minWidth: number;
+    disableOneColumnMode: boolean;
+    oneColumnModeClass: string;
+    placeholderClass: string;
+    placeholderText: string;
+    resizable: boolean;
+    removable: boolean;
+    removeTimeout: number;
+    rtl: boolean | "auto";
+    staticGrid: boolean;
+    verticalMargin: number;
+    width: number;
+}
